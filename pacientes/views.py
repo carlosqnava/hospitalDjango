1a86db1c2b18e9ea72c4673e639c8d73e80d517c
Alex Mauricio <amgdark@uaz.edu.mx>
2020-02-25 11:05:39 -0600

    Agrega Dashboard al proyecto

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from .models import Paciente
 
 
 class Lista(ListView):
     model = Paciente
+
+
+class Nuevo(CreateView):
+    pass
